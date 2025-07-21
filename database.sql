ad9f3836d9bd93a183be39a07afc2154c2cd22f1
Albert Kipchirchir <albertkipchirchir02@gmail.com>
2025-07-21 16:37:43 +0300

    seeding data

--- database.sql
+++ database.sql
@@ -41,4 +41,30 @@ CREATE TABLE farmer_batches (
 );
 
 
+INSERT INTO crops (name, nursery_days, days_to_harvest, harvest_duration_days)
+VALUES 
+('Cabbage', 25, 70, 0),
+('Capsicum', 30, 75, 30),
+('Hot Chilli', 35, 90, 365),
+('Purple Cabbage', 25, 70, 0),
+('Onions', 30, 120, 0);
 
+
+
+INSERT INTO farmers (name, phone, email,password, location)
+VALUES 
+('Alice Mwangi', '0700123456', 'alice@example.com','kenya', 'Kiambu'),
+('John Otieno', '0711223344', 'john@example.com','kenya', 'Kisumu');
+
+
+INSERT INTO batches (crop_id, variety, date_sown, expected_transplant_date, expected_harvest_start, expected_harvest_end, total_seedlings)
+VALUES 
+(1, 'Green Express', '2025-06-01', DATE_ADD('2025-06-01', INTERVAL 25 DAY), DATE_ADD('2025-06-01', INTERVAL 95 DAY), DATE_ADD('2025-06-01', INTERVAL 95 DAY), 1000),
+(2, 'California Wonder', '2025-06-10', DATE_ADD('2025-06-10', INTERVAL 30 DAY), DATE_ADD('2025-06-10', INTERVAL 105 DAY), DATE_ADD('2025-06-10', INTERVAL 135 DAY), 800),
+(3, 'Bird Eye', '2025-06-05', DATE_ADD('2025-06-05', INTERVAL 35 DAY), DATE_ADD('2025-06-05', INTERVAL 125 DAY), DATE_ADD('2025-06-05', INTERVAL 490 DAY), 1200);
+
+INSERT INTO farmer_batches (farmer_id, batch_id, seedlings_bought, date_purchased)
+VALUES 
+(1, 1, 200, '2025-06-20'),
+(2, 2, 150, '2025-07-01'),
+(1, 3, 300, '2025-07-02');
